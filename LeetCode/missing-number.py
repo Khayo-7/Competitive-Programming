692b07c949fb435790b7b09c918b285f4a140529
Khalid Mohammed <khayo7moh@gmail.com>
2024-02-09 22:36:55 -0800

    missing-number solved

---
+++ LeetCode/missing-number.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        return n*(n + 1) // 2 - sum(nums)
+        # return len(nums)*(len(nums) + 1) // 2 - sum(nums)
